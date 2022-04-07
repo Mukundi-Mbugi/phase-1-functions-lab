69a5129e7c6ee5bbda221a5d0ae7fc729393af04
Elijah Mbugi <mukundielijah@gmail.com>
2022-04-07 15:54:34 +0300

    Functions lab test

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+let headQuarterBlock=42
+let pickUpLocation=0;
+function distanceFromHqInBlocks(pickUpLocation){
+    let distanceInBlocks=Math.abs((pickUpLocation-headQuarterBlock))
+    return distanceInBlocks
+}
+// console.log(distanceFromHqInBlocks(10))
+function distanceFromHqInFeet(pickUpLocation){
+    let distanceInFeet=Math.abs((pickUpLocation-headQuarterBlock))*264
+    return distanceInFeet
+}
+function distanceTravelledInFeet(start,end){
+    return Math.abs((end-start))*264
+}
+
+function calculatesFarePrice(start, destination){
+    let distanceTravelledInFeet=Math.abs((destination-start))*264
+    let farePrice;
+    if(distanceTravelledInFeet<=400){
+        farePrice=0
+    }
+    else if(distanceTravelledInFeet>400&&distanceTravelledInFeet<=2000){
+     farePrice=((distanceTravelledInFeet-400)*2)/100
+    }
+    else if(distanceTravelledInFeet>2000&&distanceTravelledInFeet<2500){
+        farePrice=25
+    }
+    else if(distanceTravelledInFeet>2500){
+        return `cannot travel that far`
+    }
+    return farePrice
+    
+}
+console.log(calculatesFarePrice(34,24))
